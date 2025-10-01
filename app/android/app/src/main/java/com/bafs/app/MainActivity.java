053e32b1bb9376f19127709177c82c5220ecb963
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-10-01 15:34:02 +0200

    wip

---
+++ app/android/app/src/main/java/com/bafs/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bafs.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bafs.app',
+  appName: 'Bluetooth Arduino File Sharing',
+  webDir: 'build'
+};
+
+export default config;
